e5c1320c271ed3c4368a2b3080ea4c80fbe8b272
Sasa Jovanovic <1303sasa@gmail.com>
2022-07-12 10:29:50 +0200

    0:FW

---
+++ SasoFramewoork/SasoFramewoork/SasoFramewoork.h
@@ -0,0 +1,18 @@
+//
+//  SasoFramewoork.h
+//  SasoFramewoork
+//
+//  Created by Sasa Jovanovic on 12.7.22..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SasoFramewoork.
+FOUNDATION_EXPORT double SasoFramewoorkVersionNumber;
+
+//! Project version string for SasoFramewoork.
+FOUNDATION_EXPORT const unsigned char SasoFramewoorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SasoFramewoork/PublicHeader.h>
+
+
---
+++ SasoFramewoork/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  SasoFramewoork
+//
+//  Created by Sasa Jovanovic on 12.7.22..
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomeStuff() -> String {
+        return "Olaa komo estas"
+    }
+    
+    
+}
